e2c12c7e7f7eef003e1028811eee04406e4c7999
MargretPetrova <margret.ruseva@gmail.com>
2022-08-04 23:43:55 +0300

    css changes

--- SPA/src/components/AboutPage/About.js
+++ SPA/src/components/AboutPage/About.js
@@ -9,29 +9,26 @@ export default function About() {
     
 
     return (
-        <main>
+        <main className={styles.body}>
             <section className={styles.backgroundImage}>
                 <div className={styles.aboutContainer}>
 
                     <div className={styles.info}>
                         <h1>About this site</h1>
-
-                        
                     </div>
-
                 </div>
-                
             </section>
             <div className={styles.paragraf}>
     <p>This site is for support for children and families of Ukraine.
-                            There are many actions we can take as individuals to help Ukrainians in the country, those on the move and internally displaced, and those who have fled the country.
-                            The first step anyone can take to help is to learn about the issue and continue to stay informed. In a digital age that gives us infinite access to information, we can also be vulnerable to disinformation, so make sure the sources you are looking into are verified and trusted.
-                            A simple steps you can take to help  is by opening 
-                            <Link to="/create" className={styles.links}> OWN HELP CENTER</Link>
+                            There are many actions we can take as individuals to help Ukrainians who have fled the country.
+                            <p>The first step anyone can take to help is to learn about the issue and continue to stay informed.
+                                </p>
+                                <p>A simple steps you can take to help  is by opening </p>
+                            <Link to="/create" className={styles.links}>   OWN HELP CENTER  </Link>
                             , become a
-                            <Link to="/catalog" className={styles.links}> VOLUNTEER </Link>
+                            <Link to="/catalog" className={styles.links}>   VOLUNTEER   </Link>
                             or
-                            <Link to="/catalog" className={styles.links}> DONATING </Link>
+                            <Link to="/catalog" className={styles.links}>   DONATING   </Link>
                             in centers registered in our site.
                         </p>
                         </div>
